c590d33600eb23ad9ab59c052fb85e8f54c87b7e
CalvinP4 <calvincp28@gmail.com>
2024-04-10 14:32:48 -0400

    Update the dataset

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 let data, barChart, lineChart;
 
-d3.csv('data/FinalProjectOutputV4.csv').then(_data => {
+d3.csv('data/FinalProjectOutput.csv').then(_data => {
     data = _data.map(d => {
-        const keys = ['State', 'Area_name', 'Civilian_labor_force', 'Employed', 'Unemployed', 'Unemployment_rate', 'Year', 'state', 'areaname', 'county', 'family_member_count', 'housing_cost', 'food_cost', 'transportation_cost', 'healthcare_cost', 'other_necessities_cost', 'childcare_cost', 'taxes', 'total_cost', 'median_family_income'
+        const keys = ['F1','case_id','state','isMetro',
+        'areaname','county','family_member_count','housing_cost',
+        'food_cost','transportation_cost','healthcare_cost',
+        'other_necessities_cost','childcare_cost','taxes',
+        'total_cost','median_family_income','date',
+        'State','Area_name','Civilian_labor_force','Employed','Unemployed',
+        'Unemployment_rate','Year'
         ];
 
-        // keys.forEach(key => {
-        //     d[key] = d[key] === "NA" ? null : +d[key];
-        // });
 
         return d;
     }).filter(d => {
