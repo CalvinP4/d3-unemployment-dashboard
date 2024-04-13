e3a59a09e7de09c1da40c9f04020a84b7771e543
Jullu11 <Jaineshlad1228@gmail.com>
2024-04-12 22:54:34 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ let data, barChart, lineChart;
 
 d3.csv('data/FinalProjectOutput.csv').then(_data => {
     data = _data.map(d => {
-        const numericKeys = ['family_member_count','housing_cost',
-        'food_cost','transportation_cost','healthcare_cost',
-        'other_necessities_cost','childcare_cost','taxes',
-        'total_cost','median_family_income', 'Civilian_labor_force','Employed','Unemployed',
-        'Unemployment_rate'];
+        const numericKeys = ['family_member_count', 'housing_cost',
+            'food_cost', 'transportation_cost', 'healthcare_cost',
+            'other_necessities_cost', 'childcare_cost', 'taxes',
+            'total_cost', 'median_family_income', 'Civilian_labor_force', 'Employed', 'Unemployed',
+            'Unemployment_rate'];
 
         numericKeys.forEach(key => {
             d[key] = d[key] === "NA" ? null : +d[key];
         });
-        
+
         return d;
     }).filter(d => {
         return !Object.values(d).includes(null);
@@ -21,10 +21,10 @@ d3.csv('data/FinalProjectOutput.csv').then(_data => {
 
     const colorScale = d3.scaleOrdinal().domain(isMetro).range(d3.schemeCategory10);
 
-    barchart = new BarChart({parentElement: '#barchart'},data, colorScale);
+    barchart = new BarChart({ parentElement: '#barchart' }, data, colorScale);
     barchart.updateVis();
 
-    lineChart = new LineChart({parentElement: '#linechart'}, data, colorScale);
+    lineChart = new LineChart({ parentElement: '#linechart' }, data, colorScale);
     lineChart.updateVis();
     console.log(data);
 });
